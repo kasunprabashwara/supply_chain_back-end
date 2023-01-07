97274e0c0dec23590fb05f55f85076dd48ff8f23
Kasun Prabashwara <kasunprabashwara24@gmail.com>
2023-01-08 00:55:42 +0530

    can save and validate users

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
@@ -27,6 +30,67 @@ app.post("/api/insert", (req, res) => {
   db.query(sqlInsert, [cusName, review], (err, result) => {
     console.log(err);
   });
+});
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * FROM login_data WHERE email = ?",
+    [email],
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      }
+      if (result.length == 0) {
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+          db.query(
+            "INSERT INTO login_data (email, password) VALUE (?,?)",
+            [email, hash],
+            (error, response) => {
+              if (err) {
+                res.send(err);
+              }
+
+              res.send({ msg: "User successfully added" });
+            }
+          );
+        });
+      } else {
+        res.send({ msg: "Email already exists" });
+      }
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * FROM login_data WHERE email = ?",
+    [email],
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      }
+      if (result.length > 0) {
+        bcrypt.compare(password, result[0].password, (error, response) => {
+          if (error) {
+            res.send(error);
+          }
+          if (response) {
+            res.send({ msg: "User logged in" });
+          } else {
+            res.send({ msg: "Incorrect password" });
+          }
+        });
+      } else {
+        res.send({ msg: "User not registered" });
+      }
+    }
+  );
 });
 
 app.get("/", (req, res) => {
